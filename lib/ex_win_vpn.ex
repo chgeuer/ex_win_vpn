453cfe5a89588a43e69fbafdf9295ba3e9171a14
Dr. Christian Geuer-Pollmann <chgeuer@users.noreply.github.com>
2024-10-18 17:23:12 +0200

    Initial checkin

---
+++ lib/ex_win_vpn.ex
@@ -0,0 +1,18 @@
+defmodule ExWinVpn do
+  @moduledoc """
+  Documentation for `ExWinVpn`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExWinVpn.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_win_vpn_test.exs
@@ -0,0 +1,8 @@
+defmodule ExWinVpnTest do
+  use ExUnit.Case
+  doctest ExWinVpn
+
+  test "greets the world" do
+    assert ExWinVpn.hello() == :world
+  end
+end
