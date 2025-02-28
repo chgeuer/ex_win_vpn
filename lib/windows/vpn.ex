fd6ef53d4e32753fd9c240bce655cc3e946e47cd
Dr. Christian Geuer-Pollmann <chgeuer@users.noreply.github.com>
2025-02-28 09:59:48 +0100

    Update vpn.ex

--- lib/windows/vpn.ex
+++ lib/windows/vpn.ex
@@ -18,4 +18,33 @@ defmodule Windows.VPN do
         {:error, error_code, output}
     end
   end
+
+
+  @no_connections "No connections"
+  @connected "Connected to"
+  @success "Command completed successfully."
+  @doc """
+  Checks current VPN connections using rasdial.exe and returns a tuple indicating status.
+  
+  Returns:
+  - {:connected, [connection_names]} - when VPN connections are active
+  - {:disconnected} - when no VPN connections are active
+  """
+  def list_connections do
+    with {response, 0} <- System.cmd("rasdial.exe", [], stderr_to_stdout: true),
+      lines <- response |> String.split("\n", trim: true) do
+      lines
+      |> case do
+        [ @no_connections, @success] ->
+          {:disconnected}
+          
+        [@connected | rest_with_command] ->
+          {connection_names, [@success]} = Enum.split(rest_with_command, -1)
+          {:connected, connection_names}
+          
+        _ ->
+          {:error, "rasdial.exe returned unknown response: #{response}"}
+      end
+    end 
+  end
 end
